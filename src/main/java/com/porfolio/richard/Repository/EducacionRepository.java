861d05cd096d27cd6f1bbcf54a424ae9bc937c86
Ricardo Canteros <richardlihue@gmail.com>
2022-07-21 12:33:09 -0300

    cambio

--- src/main/java/com/porfolio/richard/Repository/EducacionRepository.java
+++ src/main/java/com/porfolio/richard/Repository/EducacionRepository.java
@@ -3,8 +3,11 @@ package com.porfolio.richard.Repository;
 import com.porfolio.richard.Entity.Educacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface EducacionRepository extends JpaRepository<Educacion,Long> {
+ Optional<Educacion> findByTituloEdu(String np);
+    boolean existsByTituloEdu(String np);
 
 }
