8c73eb8011a7eb568f42ecd212ac400ceb97989d
Ricardo Canteros <richardlihue@gmail.com>
2022-07-21 02:06:34 -0300

    nnn

--- src/main/java/com/porfolio/richard/Controller/EducacionController.java
+++ src/main/java/com/porfolio/richard/Controller/EducacionController.java
@@ -35,7 +35,7 @@ public class EducacionController {
 
  
 
-    @PostMapping("nuevo")
+    @PostMapping("agregar-educacion")
     public ResponseEntity<Educacion> create(@RequestBody  Educacion educacion){
         educacionService.guardar(educacion);
         return new ResponseEntity(new Mensaje("educacion guardada"),HttpStatus.CREATED);
