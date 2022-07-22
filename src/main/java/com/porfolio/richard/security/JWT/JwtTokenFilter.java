810f05dc0cd21f08d0c0422922f5fc06f48947a4
Ricardo Canteros <richardlihue@gmail.com>
2022-07-22 10:15:07 -0300

    cambio

--- src/main/java/com/porfolio/richard/security/JWT/JwtTokenFilter.java
+++ src/main/java/com/porfolio/richard/security/JWT/JwtTokenFilter.java
@@ -44,7 +44,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     private String getToken(HttpServletRequest request){
         String authReq = request.getHeader("Authorization");
         if(authReq != null && authReq.startsWith("Bearer"))
-            return authReq.replace("Bearer", "");
+            return authReq.replace("Bearer ", "");
         return null;
     }
 }
