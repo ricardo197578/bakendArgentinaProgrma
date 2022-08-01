4b0077deb71cc643d32d281088b8a64cbe755220
Ricardo Canteros <richardlihue@gmail.com>
2022-08-01 13:19:53 -0300

    cambio

--- src/main/java/com/porfolio/richard/Repository/ExperienciaRepository.java
+++ src/main/java/com/porfolio/richard/Repository/ExperienciaRepository.java
@@ -3,6 +3,7 @@ package com.porfolio.richard.Repository;
 import com.porfolio.richard.Entity.Experiencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface ExperienciaRepository extends JpaRepository<Experiencia,Long> {
