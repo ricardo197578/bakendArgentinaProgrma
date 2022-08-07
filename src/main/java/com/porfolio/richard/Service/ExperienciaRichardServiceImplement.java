6d72800044ae2819fa46050cfcf82765442e1098
Ricardo Canteros <richardlihue@gmail.com>
2022-08-07 20:34:44 -0300

    cambio

--- src/main/java/com/porfolio/richard/Service/ExperienciaRichardServiceImplement.java
+++ src/main/java/com/porfolio/richard/Service/ExperienciaRichardServiceImplement.java
@@ -1,7 +1,7 @@
 package com.porfolio.richard.Service;
 
-import com.porfolio.richard.Repository.ExperienciaRichardRepository;
-import com.porfolio.richard.Entity.ExperienciaRichard;
+import com.porfolio.richard.Dao.ExperienciaRichardDao;
+import com.porfolio.richard.Model.ExperienciaRichard;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,36 +11,35 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ExperienciaRichardServiceImplement  implements ExperienciaRichardService{
     @Autowired
-    private ExperienciaRichardRepository experienciaRichardRepository;
+    private ExperienciaRichardDao experienciaRichardDao;
 
     @Override
     @Transactional(readOnly=true)
     public List<ExperienciaRichard> findAll()
     {
-        return (List<ExperienciaRichard>) experienciaRichardRepository.findAll();
+        return (List<ExperienciaRichard>) experienciaRichardDao.findAll();
     }
 
     @Override
      @Transactional(readOnly=false)
     public ExperienciaRichard save(ExperienciaRichard experienciaRichard)
     {
-        return experienciaRichardRepository.save(experienciaRichard);
+        return experienciaRichardDao.save(experienciaRichard);
     }
 
     @Override
      @Transactional(readOnly=true)
     public ExperienciaRichard findById(Integer id)
     {
-        return experienciaRichardRepository.findById(id).orElse(null);
+        return experienciaRichardDao.findById(id).orElse(null);
     }
 
     @Override
      @Transactional(readOnly=false)
     public void delete(Integer id)
     {
-        experienciaRichardRepository.deleteById(id);
+        experienciaRichardDao.deleteById(id);
     }
 
 }
-
 
