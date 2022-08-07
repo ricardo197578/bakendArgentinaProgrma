cfc277860ee0f62e34864919539415114b5ed50b
Ricardo Canteros <richardlihue@gmail.com>
2022-08-07 19:58:36 -0300

    cambio

--- src/main/java/com/porfolio/richard/Controller/ExperienciaRichardController.java
+++ src/main/java/com/porfolio/richard/Controller/ExperienciaRichardController.java
@@ -1,6 +1,6 @@
 package com.porfolio.richard.Controller;
 
-import com.porfolio.richard.Model.ExperienciaRichard;
+import com.porfolio.richard.Entity.ExperienciaRichard;
 import com.porfolio.richard.Service.ExperienciaRichardService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import org.springframework.http.HttpStatus;
+
 
 @RestController
 @CrossOrigin(origins={"*"})
@@ -30,6 +34,7 @@ public class ExperienciaRichardController {
 
     //guardar
     @PostMapping("/experiencia")
+    // @PreAuthorize("hasRole('ADMIN')")
     public  ExperienciaRichard guardar(@RequestBody ExperienciaRichard experienciaRichard)
     {
         return experienciaRichardService.save(experienciaRichard);
@@ -37,6 +42,8 @@ public class ExperienciaRichardController {
 
     //get una tarea
     @GetMapping("/experiencia{id}")
+   // @PreAuthorize("hasRole('ADMIN')")
+
     public ExperienciaRichard getUnaExperienciaRichard(@PathVariable Integer id)
     {
         return experienciaRichardService.findById(id);
@@ -44,23 +51,29 @@ public class ExperienciaRichardController {
 
     //Modeficar
     @PutMapping("/experiencia/{id}")
+    //@PreAuthorize("hasRole('ADMIN')")
+
     public ExperienciaRichard modifecar(@RequestBody ExperienciaRichard experienciaRichard,@PathVariable Integer id)
     {
         ExperienciaRichard experienciaRichardActual= experienciaRichardService.findById(id);
         experienciaRichardActual.setEmpresa(experienciaRichard.getEmpresa());
         experienciaRichardActual.setFechaInicioExp(experienciaRichard.getFechaInicioExp());
-	experienciaRichardActual.setFechaFinExp(experienciaRichard.getFechaFinExp());
-	experienciaRichardActual.setDescripcionExp(experienciaRichard.getDescripcionExp());
-	experienciaRichardActual.setImagenExp(experienciaRichard.getImagenExp());
+        experienciaRichardActual.setFechaFinExp(experienciaRichard.getFechaFinExp());
+        experienciaRichardActual.setDescripcionExp(experienciaRichard.getDescripcionExp());
+        experienciaRichardActual.setImagenExp(experienciaRichard.getImagenExp());
 
         return experienciaRichardService.save(experienciaRichardActual);
     }
 
     @DeleteMapping("/experiencia/{id}")
-    public void eliminar(@PathVariable Integer id)
+   // @PreAuthorize("hasRole('ADMIN')")
+
+        public void eliminar(@PathVariable Integer id)
     {
         experienciaRichardService.delete(id);
     }
 
 }
+
+
 
--- src/main/java/com/porfolio/richard/Service/ExperienciaRichardServiceImplement.java
+++ src/main/java/com/porfolio/richard/Service/ExperienciaRichardServiceImplement.java
@@ -1,7 +1,7 @@
 package com.porfolio.richard.Service;
 
-import com.porfolio.richard.Dao.ExperienciaRichardDao;
-import com.porfolio.richard.Model.ExperienciaRichard;
+import com.porfolio.richard.Repository.ExperienciaRichardRepository;
+import com.porfolio.richard.Entity.ExperienciaRichard;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,35 +11,36 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ExperienciaRichardServiceImplement  implements ExperienciaRichardService{
     @Autowired
-    private ExperienciaRichardDao experienciaRichardDao;
+    private ExperienciaRichardRepository experienciaRichardRepository;
 
     @Override
     @Transactional(readOnly=true)
     public List<ExperienciaRichard> findAll()
     {
-        return (List<ExperienciaRichard>) experienciaRichardDao.findAll();
+        return (List<ExperienciaRichard>) experienciaRichardRepository.findAll();
     }
 
     @Override
      @Transactional(readOnly=false)
     public ExperienciaRichard save(ExperienciaRichard experienciaRichard)
     {
-        return experienciaRichardDao.save(experienciaRichard);
+        return experienciaRichardRepository.save(experienciaRichard);
     }
 
     @Override
      @Transactional(readOnly=true)
     public ExperienciaRichard findById(Integer id)
     {
-        return experienciaRichardDao.findById(id).orElse(null);
+        return experienciaRichardRepository.findById(id).orElse(null);
     }
 
     @Override
      @Transactional(readOnly=false)
     public void delete(Integer id)
     {
-        experienciaRichardDao.deleteById(id);
+        experienciaRichardRepository.deleteById(id);
     }
 
 }
+
 
