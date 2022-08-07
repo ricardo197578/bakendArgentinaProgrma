088f915ea8674f8d7034aa8a2bd3795bab37ff80
Ricardo Canteros <richardlihue@gmail.com>
2022-08-07 14:12:31 -0300

    cambio

--- src/main/java/com/porfolio/richard/Repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.porfolio.richard.Repository;
-
-import com.porfolio.richard.Entity.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends JpaRepository<Persona,Long>{}
-
--- src/test/java/com/codeteam/CrudCode/CrudCodeApplicationTests.java
+++ src/test/java/com/codeteam/CrudCode/CrudCodeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.porfolio.richard;
+package com.codeteam.CrudCode;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RichardApplicationTests {
+class CrudCodeApplicationTests {
 
 	@Test
 	void contextLoads() {
