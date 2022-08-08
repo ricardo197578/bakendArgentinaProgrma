7ec9b96a3d4cc0f0a94c34ed6b26a5584121b9ef
Ricardo Canteros <richardlihue@gmail.com>
2022-08-08 20:33:19 -0300

    cambio

--- src/main/java/com/porfolio/richard/Security/MainSecurity.java
+++ src/main/java/com/porfolio/richard/Security/MainSecurity.java
@@ -41,7 +41,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("**").permitAll()
+                .antMatchers("/api/auth/**","/api/experiencia").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
