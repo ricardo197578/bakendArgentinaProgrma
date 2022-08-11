b3e083e36e5dbf1512870c29822900fae8da80d1
Ricardo Canteros <richardlihue@gmail.com>
2022-08-10 21:45:35 -0300

    cambio

--- src/main/java/com/porfolio/richard/repository/RolRepository.java
+++ src/main/java/com/porfolio/richard/repository/RolRepository.java
@@ -1,6 +1,6 @@
-package com.richard.porfolio.repository;
+package com.porfolio.richard.repository;
 
-import com.richard.porfolio.entity.Rol;
+import com.porfolio.richard.entity.Rol;
 import com.porfolio.richard.enums.RolNombre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
