7a1a9e05b98f81ea22531165973b228dc5d768b1
Ricardo Canteros <richardlihue@gmail.com>
2022-08-16 22:56:17 -0300

    cambio

--- src/main/java/com/porfolio/richard/security/WebSecurity.java
+++ src/main/java/com/porfolio/richard/security/WebSecurity.java
@@ -58,8 +58,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**","/api/tareas/**","/api/educacion/**","/api/experiencia/**").permitAll()
-               // .antMatchers("/api/auth/**").permitAll()
+               // .antMatchers("/api/auth/**","/api/tareas/**","/api/educacion/**","/api/experiencia/**").permitAll()
+                .antMatchers("**").permitAll()
 
 		.anyRequest().authenticated()
                 .and()
